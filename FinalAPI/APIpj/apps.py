cc78460e68be9aa90cdf901edcc7a7a385124d04
PavlovAA8 <artem00p@gmail.com>
2025-08-11 00:32:53 +0300

    Revert "Migration fault"
    This reverts commit f38596af2f6c6df1487efeb45d6caaa2b6fffa32.

--- FinalAPI/APIpj/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class ApipjConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'APIpj'
--- FinalAPI/FinalAPI/settings.py
+++ FinalAPI/FinalAPI/settings.py
@@ -40,7 +40,6 @@ INSTALLED_APPS = [
     'rest_framework',
     'FinalAPI',
     'rest_framework_swagger',
-    'APIpj',
 ]
 
 MIDDLEWARE = [
@@ -120,9 +119,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.2/topics/i18n/
 
-LANGUAGE_CODE = 'ru'
+LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'Europe/Moscow'
+TIME_ZONE = 'UTC'
 
 USE_I18N = True
 
@@ -154,9 +153,3 @@ REST_FRAMEWORK ={
         'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
     ]
 }
-
-LOCAL_APPS = [
-    'APIpj',
-]
-
-AUTH_USER_MODEL = 'APIpj.User'
